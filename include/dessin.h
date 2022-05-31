6067d3be85184d82984404928ea4964f891d5f68
Imad <imad.allal@outlook.com>
2022-05-31 12:59:35 +0200

    Cirle & Disk functions

--- include/dessin.h
+++ include/dessin.h
@@ -3,7 +3,10 @@
 
 #include <SDL2/SDL.h>
 
-void dessinerLigne(int, int, int, int, SDL_Renderer *);
-void rectangle(int, int, int, int, int, int, int, int, SDL_Renderer *);
+void dessinerLigne(int, int, int, int, SDL_Renderer*);
+void rectangle(int, int, int, int, int, int, int, int, SDL_Renderer*);
+void remplirRct(int, int, int, int, SDL_Renderer*);
+void cercle(int, int, int, SDL_Renderer *);
+void cercle(int, int, int, SDL_Renderer *);
 
 #endif
