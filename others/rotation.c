81af6762590a17de5c3e692b61436e9c41df0867
Imad <imad.allal@outlook.com>
2022-06-07 01:21:11 +0200

    rotation.c upgraded

--- others/rotation.c
+++ others/rotation.c
@@ -99,13 +99,14 @@ void algoNaif(int x0, int y0, int x1, int y1){
 
 void rotate (int x, int y, int cx, int cy, double angle){
     int x1, y1;
-    points2[n].x = cx + (x - cx) * cos(angle) - (y - cy) * sin(angle);
-    points2[n].y = cy + (x - cy) * sin(angle) + (y - cy) * cos(angle);
+    points[n].x = Round(cx + (x - cx) * cos(angle) - (y - cy) * sin(angle));
+    points[n].y = Round(cy + (x - cy) * sin(angle) + (y - cy) * cos(angle));
 }
 
 int main(int argc, char *argv[])
 {
     int cx, cy;
+    double angle = 0.1;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
@@ -135,21 +136,21 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    points[0].x = 500;
-    points[0].y = 300;
-    points[1].x = 300;
-    points[1].y = 300;
-    points[2].x = 300;
-    points[2].y = 400;
-    points[3].x = 500;
-    points[3].y = 400;
+    points[0].x = 300;
+    points[0].y = 400;
+    points[1].x = 500;
+    points[1].y = 400;
+    points[2].x = 500;
+    points[2].y = 300;
+    points[3].x = 300;
+    points[3].y = 300;
 
     for (int i = 0; i<N; i++){
-        if(i == N-1){ 
-            algoNaif(points[i].x, points[i].y, points[0].x, points[0].y);
-        }
-        else
-            algoNaif(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+    if(i == N-1){ 
+        algoNaif(points[i].x, points[i].y, points[0].x, points[0].y);
+    }
+    else
+        algoNaif(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
     }
 
     SDL_RenderPresent(renderer);
@@ -159,21 +160,41 @@ int main(int argc, char *argv[])
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    cx = (points[1].x - points[0].x) / 2 + points[0].x;
-    cy = (points[2].y - points[1].y) / 2 + points[1].y;
+    cx = ((points[1].x - points[0].x) / 2) + points[0].x;
+    cy = ((points[2].y - points[1].y) / 2) + points[1].y;
+    printf("cx = %d cy = %d\n", cx, cy);
 
-    for (int i = 0; i<N; i++){
-        rotate(points[i].x, points[i].y, cx, cy, 0.1259);
-        n++;
-        printf("%d, %d\n", points2[i].x, points2[i].y);
+    while(angle < 0.175){
+        for (int i = 0; i<N; i++){
+            if(i == N-1){ 
+                algoNaif(points[i].x, points[i].y, points[0].x, points[0].y);
+            }
+            else
+                algoNaif(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(15);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+
+
+        for (int i = 0; i<N; i++){
+            rotate(points[i].x, points[i].y, cx, cy, angle);
+            n++;
+        }
+        n = 0;
+        angle += 0.001;
     }
-
     for (int i = 0; i<N; i++){
         if(i == N-1){ 
-            algoNaif(points2[i].x, points2[i].y, points2[0].x, points2[0].y);
+            algoNaif(points[i].x, points[i].y, points[0].x, points[0].y);
         }
         else
-            algoNaif(points2[i].x, points2[i].y, points2[i+1].x, points2[i+1].y);
+            algoNaif(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
     }    
     SDL_RenderPresent(renderer);
 
