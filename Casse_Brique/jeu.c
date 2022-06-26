6cab40b3d7fe89a07a7275c50717d83eeac46e10
Imad <imad.allal@outlook.com>
2022-06-27 01:59:42 +0200

    game almost ready

--- Casse_Brique/jeu.c
+++ Casse_Brique/jeu.c
@@ -24,20 +24,19 @@ SDL_Point tr[2][3] = {
      {750, 0},
      {1500, 450}},
 };
-struct Rectangle rec[SIZE];
 
-SDL_Point palette[4] = {{600,1370},
-                        {900, 1370},
-                        {900,1400}, 
-                        {600,1400}
-                        };
+struct Rectangle rec[SIZE];
+struct Rectangle palette = {600, 900, 1370, 1400, 750,1885};
+
+const Uint8 *clavier;
+
 int supprimerRec[SIZE];
-int taille = 0;
 int xmin, xmax, ymin, ymax;
-int x, y;
-int ux =5, uy = 5; 
-int r = 25;
+int ux =5, uy = 5; // Vecteurs de translation
+int taille = 0; // taille du buffer contenant les blocs touchés
+int r = 25; // Rayon du disque
 int j = 0;
+int x, y;
 
 void quit(){
     if (renderer != NULL)
@@ -85,7 +84,7 @@ void horizontale (int x0, int y0, int dx){
     }  
 }
 
-void algoNaif(int x0, int y0, int x1, int y1){
+void algoNaif(int x0, int y0, int x1, int y1){ //Tracage de lignes
     float i, x, y;
     int dx = x1 - x0;
     int dy = y1 - y0;
@@ -116,30 +115,13 @@ void algoNaif(int x0, int y0, int x1, int y1){
     }
 }
 
-void pal(){
-    int xmin = WIDTH, xmax = 0, ymin = HEIGHT, ymax = 0;
-    for (int i = 0; i < 4; i++)
-    {
-        if (palette[i].x >= xmax){
-            xmax = palette[i].x;
-        }
-        if (palette[i].x <= xmin){
-            xmin = palette[i].x;
-        }
-        if (palette[i].y >= ymax){
-            ymax = palette[i].y;
-        }
-        if (palette[i].y <= ymin){
-            ymin = palette[i].y;
-        }
-
-    }
-    for (int i = ymin; i <= ymax; i++){
-        horizontale(xmin, i, xmax - xmin);
+void pal(){ // Fonction pour palette
+    for (int i = palette.ymin; i <= palette.ymax; i++){
+        horizontale(palette.xmin, i, palette.xmax - palette.xmin);
     }
 }
 
-void triangle(int x0, int x1, int y0, int y1){
+void triangle(int x0, int x1, int y0, int y1){ // Triangles verts en haut
     float i, x, y, m;
     int dx = x1 - x0;
     int dy = y1 - y0;
@@ -164,27 +146,7 @@ void triangle(int x0, int x1, int y0, int y1){
     }
 }
 
-void droiteG(int x0, int y0, int x1, int y1, int i){
-    float x, y, m;
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    
-    m = ((float)dy) / ((float)dx);
-    p_droite.x = x0 + i;
-    p_droite.y = Round(y0 + m * i);
-}
-
-void droiteD(int x0, int y0, int x1, int y1, int i){
-    float x, y, m;
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-    
-    m = ((float)dy) / ((float)dx);
-    p_droite.x = x0 + i;
-    p_droite.y = Round(y0 + m * i);
-}
-
-void disque(int cx, int cy){
+void disque(int cx, int cy){ // Tracage de disque (balle)
     int x0 = cx - r;
     int x1 = cx + r;
     int x = x1;
@@ -202,7 +164,7 @@ void disque(int cx, int cy){
     }
 }
 
-void remplirRec(int x0, int x1, int y0, int y1){
+void remplirRec(int x0, int x1, int y0, int y1){ // Remplissage de rectangles
     int y = y0;
     int dx = x1 - x0;
     while (y < y1)
@@ -218,7 +180,7 @@ int tmp = *a;
 *a = *b; 
 *b = tmp; 
 }   
-// Selection Sort
+// Trie de tableau par selection
 void trierTab(int tab[], int n) 
 { 
     int i, j, min; 
@@ -232,12 +194,12 @@ void trierTab(int tab[], int n)
     } 
 }
 
-void rectangle(){
+void rectangle(){ // Tracage de rectangles
     int cx = 50, cy = 700;
     int longeur = 100, largeur = 50;
     int cpt = 0, key = 0;
 
-    if (taille == 0)
+    if (taille == 0) // Liste de rectangles à supprimer vide ==> tracer tous les rectangles 
     {
         for (int i = 0; i < 30; i++)
         {
@@ -258,12 +220,12 @@ void rectangle(){
         }
     }
     else{
-        if (taille > 1)
+        if (taille > 1) // Ne pas tracer les rectangles contenus dans la liste
             trierTab(supprimerRec, taille);
         for (int i = 0; i < 30; i++)
         {
             key = 0;
-            while(cpt == 0 || cpt<taille){
+            while(cpt<taille){
                 if (i == supprimerRec[cpt]){
                     cx += 100;
                     key = 1;
@@ -274,9 +236,11 @@ void rectangle(){
                     break;
             }
 
-            if (key)
+            if (key){
                 continue;
-            if (i == 15)
+            }
+
+            if (i == 15) // Chaque ligne contient 15 rectangles, si on est alors dans le 15eme, on monte d'une ligne
             {
                 cx = 50;
                 cy = 650;
@@ -293,29 +257,29 @@ void rectangle(){
         }     
     }       
 }
-
-void all(){
-    SDL_SetRenderDrawColor(renderer,105,255,105,255);
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 3; j++){
-            if (j == 2)
-            {
-                triangle(tr[i][j].x, tr[i][0].x, tr[i][j].y, tr[i][0].y);
-            }
-            else{
-                triangle(tr[i][j].x, tr[i][j+1].x, tr[i][j].y, tr[i][j+1].y);
-            }
-        }
-    }
-    SDL_SetRenderDrawColor(renderer,255,255,255,255);  
-    algoNaif(0, 400, 750, 0);
-    algoNaif(1500, 400, 750, 0);
-    disque(x, y);
-    rectangle();
-    pal();
-    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+//// Les deux fonctions en dessous permettent de récuperer chaque point (x,y) des droites limitantes en haut à l'aide de l'algo naïf ////
+void droiteG(int x0, int y0, int x1, int y1, int i){ // Limite du terrain (haut à gauche)
+    float x, y, m;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    
+    m = ((float)dy) / ((float)dx);
+    p_droite.x = x0 + i;
+    p_droite.y = Round(y0 + m * i);
 }
 
+void droiteD(int x0, int y0, int x1, int y1, int i){ // Limite du terrain (haut à droite)
+    float x, y, m;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    
+    m = ((float)dy) / ((float)dx);
+    p_droite.x = x0 + i;
+    p_droite.y = Round(y0 + m * i);
+}
+//// Les deux fonctions au dessus permettent de récuperer chaque point (x,y) des droites limitantes en haut à l'aide de l'algo naïf ////
+
+//// Les deux fonctions en dessous permettent de récuperer chaque point (x,y) de la moitié haute de notre disque et les comparer avec les point (x,y) des droites limitantes en haut ////
 int toitG(int xc, int yc){
     SDL_Point t;
     int x0 = xc - r;
@@ -329,9 +293,9 @@ int toitG(int xc, int yc){
         y0 = ball.y + sqrt((r * r) - (x0 - ball.x) * (x0 - ball.x));
         t.x = x0;
         t.y = yc;
-        for (int i = 0; i < 760; i++)
+        for (int i = 0; i < 750; i++)
         {
-            droiteG(0, 430, 750, 30,i);
+            droiteG(0, 425, 750, 25,i);
             if (t.x == p_droite.x && t.y == p_droite.y)
             {
                 return 1;
@@ -357,7 +321,7 @@ int toitD(int xc, int yc){
         t.x = x0;
         t.y = yc;
         for (int j = 0; j > -750; j--){
-            droiteD(1500, 430, 750, 30, j);
+            droiteD(1500, 425, 750, 25, j);
             if (t.x == p_droite.x && t.y == p_droite.y){
                 return 1;
             }
@@ -367,9 +331,55 @@ int toitD(int xc, int yc){
     }
     return 0;
 }
+//// Les deux fonctions au dessus permettent de récuperer chaque point (x,y) de la moitié haute de notre disque et les comparer avec les point (x,y) des droites limitantes en haut ////
 
+void rendu(){ // Crée, mis à jour et affiche notre terrain de jeu
+    SDL_SetRenderDrawColor(renderer,105,255,105,255);
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+            if (j == 2)
+            {
+                triangle(tr[i][j].x, tr[i][0].x, tr[i][j].y, tr[i][0].y);
+            }
+            else{
+                triangle(tr[i][j].x, tr[i][j+1].x, tr[i][j].y, tr[i][j+1].y);
+            }
+        }
+    }
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);  
+    algoNaif(0, 400, 750, 0);
+    algoNaif(1500, 400, 750, 0);
+    disque(x, y);
+    memset(rec, 0, sizeof(rec));
+    rectangle();
+    pal();
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+}
 
-int mouvement(){
+void bougerD(){ // Deplacer la palette à droite
+    if (palette.xmax < WIDTH){
+            palette.xmin += 10;
+            palette.xmax += 10;
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            rendu();
+            SDL_RenderPresent(renderer);
+    }
+}
+void bougerG(){ // Deplacer la palette à gauche
+        if (palette.xmin > 0){
+            palette.xmin -= 10;
+            palette.xmax -= 10;
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            rendu();
+            SDL_RenderPresent(renderer);
+        }
+}
+
+int mouvement(){ // Animation du disque dans le terrain
 
     int i = 0;
     int stop = 0;
@@ -377,149 +387,149 @@ int mouvement(){
     x_precedent = x;
     y_precedent = y;
 
-    printf("mouvement\n");
-    while (stop < 3)
-    {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        xmin = x - r;
-        ymin = y - r;
-        xmax = x + r;
-        ymax = y + r;
-        /*if (ymax >= HEIGHT+r*2){
-            quit();
-            return EXIT_SUCCESS;
-        }*/
-        for (i = 0; i < 30; i++)
-        {
-            if ((ymin == rec[i].ymax || ymax == rec[i].ymin) && x >= rec[i].xmin && x <= rec[i].xmax)
-            {
-                uy = -uy;
-                stop++;
-                supprimerRec[j] = i;
-                j++;
-                taille++;
-                break;
-            }
-            else if ((xmin == rec[i].xmax || xmax == rec[i].xmin) && y >= rec[i].ymin && y <= rec[i].ymax)
-            {
-                ux = -ux;
-                stop++;
-                supprimerRec[j] = i;
-                j++;
-                taille++;
-                break;
-            }
-        }
-            if (ymax >= HEIGHT)
-            {
-                uy = -uy;
-                stop++;
-            }
-            if (ymin <= 10)
-                uy = -uy;
+    xmin = x - r;
+    ymin = y - r;
+    xmax = x + r;
+    ymax = y + r;
 
-            if (y <= 400)
-            {
-                if(x<=750){
-                    if (toitG(x, y))
-                    { // Droite Gauche
-                        if (ux != 0)
-                        {
-                            if (y_precedent > y && uy != 0)
-                            {
-                                if (x_precedent>x){
-                                    ux = -ux;
-                                    uy = -uy;
-                                }
-                                else if (x_precedent<x)
-                                    ux = -20;{
-                                }
-                            }
-                            else if (y_precedent < y && uy != 0)
-                            {
-                                ux = 10;
-                            }
-                            else if (y_precedent == y && uy != 0){
-                                uy = -5;
-                                ux = 0;
-                            }
-                            else if (uy == 0) {
-                                uy = -5;
-                                ux = 0;
-                            }
-                        }
-                        else{
-                            ux = -5;
-                            uy = 0;
-                        }
-                    }
-                }
-                else if(x>=750){
-                    if(toitD(x,y))
-                    { // Droite Droite
-                        if(ux != 0){
-                            if (y_precedent > y && uy !=0){
-                                if (x_precedent > x){
-                                    ux = 10;
-                                }
-                                else if (x_precedent < x){
-                                    ux = -ux;
-                                    uy = -uy;
-                                }
-                            }
-                            else if (y_precedent < y && uy !=0){
-                                ux = -10;
-                            }
-                            else if (y_precedent < y && uy != 0){
-                                uy = -5;
-                                ux = 0;
-                            }
-                            else if (uy == 0) {
-                                uy = -5;
-                                ux = 0;
-                            }
-                        }
-                        else{
-                            ux = 5;
-                            uy = 0;
-                        }
-                    }
-                }
-        }
-        if (xmax >= WIDTH){
-            ux = -ux;
-            x_precedent = x;
-            y_precedent = y;
-            stop++;
-        }  
-
-        if (xmin <= 0)
-        {
-            ux = -ux;
-            x_precedent = x;
-            y_precedent = y;
-            stop++;
-        }        
-        x = x - ux ;
-        y = y - uy ;
-        printf("taille = %d\n", taille);
-        for (int i = 0; i<taille; i++){
-            printf("rec[%d] = %d\n", i, supprimerRec[i]);
-        }
-            all();
-        SDL_RenderPresent(renderer);
-
+    if (ymax >= HEIGHT+r*2){ // Balle sort du terrain en bas
+        return EXIT_FAILURE;
     }
-}
+    for (i = 0; i < 30; i++)
+    {
+        if ((ymin == rec[i].ymax || ymax == rec[i].ymin) && x >= rec[i].xmin && x <= rec[i].xmax) // Ball tape en haut ou en bas des rectangles
+        {
+            uy = -uy;
+            stop++;
+            supprimerRec[j] = i; // Sauvegarder les blocks touchés dans le buffer
+            j++;
+            taille++;
+            break;
+        }
+        else if ((xmin == rec[i].xmax || xmax == rec[i].xmin) && y >= rec[i].ymin && y <= rec[i].ymax) // Ball tape à gauche ou à droite des rectangles 
+        {
+            ux = -ux;
+            stop++;
+            supprimerRec[j] = i;
+            j++;
+            taille++;
+            break;
+        }
+    }
+    if (ymin <= 20) // Limite en haut
+        uy = -uy;
 
+    if (y <= 400) // cette condition permet d'eviter de faire des calcules inutiles dans les zones ou cy > 400 (400 = debut des droites limitants: 400 -> 0)
+    {
+        if(x<=750){ // Droite haute gauche
+            if (toitG(x, y))
+            { // Calcul de collision sur la Droite Gauche
+                if (ux != 0)
+                {
+                    if (y_precedent > y && uy != 0)
+                    {
+                        if (x_precedent>x){
+                            ux = -ux;
+                            uy = -uy;
+                        }
+                        else if (x_precedent<x)
+                            ux = -5;{
+                        }
+                    }
+                    else if (y_precedent < y && uy != 0)
+                    {
+                        ux = -5;
+                    }
+                    else if (y_precedent == y && uy != 0){
+                        uy = -5;
+                        ux = 0;
+                    }
+                    else if (uy == 0) {
+                        uy = -5;
+                        ux = 0;
+                    }
+                }
+                else{
+                    ux = -5;
+                    uy = 0;
+                }
+            }
+        }
+        else if(x>=750){ // Droite haute droite
+            if(toitD(x,y))
+            { // Calcul de collision sur la Droite Droite
+                if(ux != 0){
+                    if (y_precedent > y && uy !=0){
+                        if (x_precedent > x){
+                            ux = 5;
+                        }
+                        else if (x_precedent < x){
+                            ux = -ux;
+                            uy = -uy;
+                        }
+                    }
+                    else if (y_precedent < y && uy !=0){
+                        ux = -5;
+                    }
+                    else if (y_precedent < y && uy != 0){
+                        uy = -5;
+                        ux = 0;
+                    }
+                    else if (uy == 0) {
+                        uy = -5;
+                        ux = 0;
+                    }
+                }
+                else{
+                    ux = 5;
+                    uy = 0;
+                }
+            }
+        }
+    }
+    if (xmax >= WIDTH){ // Limite à gauche
+        ux = -ux;
+        x_precedent = x;
+        y_precedent = y;
+        stop++;
+    }
+
+    if (xmin <= 0) // Limite à droite
+    {
+        ux = -ux;
+        x_precedent = x;
+        y_precedent = y;
+        stop++;
+    }        
+
+    if ((ymax == palette.ymin && x>=palette.xmin && x <= palette.xmax) || (xmax == palette.xmin && y>=palette.ymin && x <= palette.ymax) || (xmin == palette.xmin && y>=palette.ymin && y <= palette.ymax)){ // Collision de la balle avec la palette
+        if (x<palette.cx){
+            ux -= -5;
+        }
+        else if (x>palette.cx){
+            ux += -5;
+        }
+        else
+            ux = 0;
+        uy = -uy;
+    }
+
+    x = x - ux ;
+    y = y - uy ;
+    rendu();
+
+    SDL_RenderPresent(renderer);
+
+
+}
 
 int main(int argc, char *argv[])
 {
 
-    int r = 35;
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         fprintf(stderr, "SDL creation error : %s", SDL_GetError());
@@ -535,7 +545,7 @@ int main(int argc, char *argv[])
     }
 
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE | SDL_RENDERER_PRESENTVSYNC);
     if(renderer == NULL) {
         fprintf(stderr,"Renderer error %s", SDL_GetError());
         quit();
@@ -547,46 +557,39 @@ int main(int argc, char *argv[])
         quit();
         return EXIT_FAILURE;
     }
+
+    clavier = SDL_GetKeyboardState(NULL);
+
     SDL_RenderClear(renderer);
-    // projection(o); // Projection des faces du polygone
-    // polygones(q); // Attribution des nouvelles coordonnées projetées au polygone
+    // projection(o);
+    // sphere();
+    // projection(o);
+    // polygones();
     x = ball.x;
     y = ball.y;
     int supprimerRec[SIZE];
 
-    all();
+    rendu();
+
     SDL_RenderPresent(renderer);
 
     SDL_Event event;
     SDL_bool quitter = SDL_FALSE;
 
     while(!quitter){
-        while (SDL_WaitEvent(&event)){
-            switch(event.type){
-                case SDL_KEYDOWN:
-                switch (event.key.keysym.sym) {
-                    case SDLK_a:
-                        printf("Vous avez demandé à quitter le programme\n");
-                        quitter = SDL_TRUE;
-                        quit();
-                        break;
-                    case SDLK_c:
-                        if(!mouvement())
-                            quitter = SDL_TRUE;
-                        break;
-                    default:
-                        printf("Vous avez appuyé sur la touche : %c\n", event.key.keysym.sym);
-                        continue;
-                    }
-                    break;
-                case SDL_QUIT: quitter = SDL_TRUE;
-                    quit();
-                    return EXIT_SUCCESS;
-                    break;
-                }
-            }
+        while (SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_a)
+                quitter = SDL_TRUE;
+        }
+        if(mouvement())
+            break;
+        if (clavier[SDL_SCANCODE_RIGHT]){
+            bougerD();
+        }
+        if (clavier[SDL_SCANCODE_LEFT]){
+            bougerG();
+        }
     }
-
     quit();
     return EXIT_SUCCESS;
 }
